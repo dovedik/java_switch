03017b2baac5b48bee32e0b806ae366307855d43
student <student@CR476>
2018-03-17 14:26:00 +0200

    verification positive number

--- src/org/tyaa/switchnloop/SwitchNLoop.java
+++ src/org/tyaa/switchnloop/SwitchNLoop.java
@@ -45,59 +45,61 @@ public class SwitchNLoop {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         
-        //2. Проверяем введенное число
-        int number10 = number % 10;
-        int number100 = number % 100;
-        
-        if (number100 >= 11 && number100 <= 14) {
-        //if (number100 == 11 || number100 == 12 || number100 == 13 || number100 == 14) {
-            System.out.println("ворон");
+        if (number < 0) {
+            System.out.println("Неверное число!");
         } else {
-            //test
-            //System.out.println("number10 = " + number10);
+            //2. Проверяем введенное число
+            int number10 = number % 10;
+            int number100 = number % 100;
 
-            //3. Выбираем слово с правильным окончанием
-            switch(number10){
-                case 0:{
-                    System.out.println("ворон");
-                    break;
-                }
-                case 1:{
-                    System.out.println("ворона");
-                    break;
-                }
-                case 2:{
-                }
-                case 3:{
+            if (number100 >= 11 && number100 <= 14) {
+            //if (number100 == 11 || number100 == 12 || number100 == 13 || number100 == 14) {
+                System.out.println("ворон");
+            } else {
+                //test
+                //System.out.println("number10 = " + number10);
 
-                }
-                case 4:{
-                    System.out.println("вороны");
-                    break;
-                }
-                case 5:{
+                //3. Выбираем слово с правильным окончанием
+                switch(number10){
+                    case 0:{
+                        System.out.println("ворон");
+                        break;
+                    }
+                    case 1:{
+                        System.out.println("ворона");
+                        break;
+                    }
+                    case 2:{
+                    }
+                    case 3:{
 
-                }
-                case 6:{
+                    }
+                    case 4:{
+                        System.out.println("вороны");
+                        break;
+                    }
+                    case 5:{
 
-                }
-                case 7:{
+                    }
+                    case 6:{
 
-                }
-                case 8:{
+                    }
+                    case 7:{
 
-                }
-                case 9:{
-                    System.out.println("ворон");
-                }
-                default:{
-                
-                    System.out.println("Неверное число!");
+                    }
+                    case 8:{
+
+                    }
+                    case 9:{
+                        System.out.println("ворон");
+                    }
+                    default:{
+
+                        System.out.println("Неверное число!");
+                    }
                 }
             }
-        }
-        
-        
+        }        
     }
     
 }
